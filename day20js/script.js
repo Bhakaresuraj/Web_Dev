2e86fa4eb6ffdfd37498254bfb4a840b2da5be6b
Bhakaresuraj <surajbhakare190@gmail.com>
2025-10-30 07:41:06 +0530

    First java script code

---
+++ day20js/script.js
@@ -0,0 +1,14 @@
+alert("Button clicked!");
+console.log("well done");
+console.log("you are writing java script code");
+console.log("Hello Suraj ");
+console.log("Wow! you are doing great");
+var a=prompt("Enter your name");
+var isTrue=confirm("Are you sure you want to Exit?");
+if(isTrue){
+    console.log("User wants to exit");
+}else{
+    console.log("User wants to stay");
+}
+console.log("Your name is "+a);
+document.title="first js code";
