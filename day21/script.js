25c0a702867e9805f2548641e846c5bb732032d7
Bhakaresuraj <surajbhakare190@gmail.com>
2025-11-16 19:59:34 +0530

    modified

---
+++ day21/script.js
@@ -0,0 +1,33 @@
+console.log("Hello suraj");
+let age=20;
+if(age > 20){
+    console.log("You are above 20");
+
+}else{
+    console.log("You are 20 or below");
+}
+age=30;
+console.log("age ",age,"ok");
+var a="suraj";
+
+console.log(typeof(a));
+a=20
+console.log(a);
+console.log(typeof(a));
+
+const dictionary = {
+    name : "Suraj"
+}
+console.log(dictionary);
+dictionary.name=300;
+dictionary.city='pune';
+console.log(dictionary);
+
+
+const dictio ={
+    'eat' :"eat",
+    'avoid' :"Ignore",
+   'sold' :"sell"
+
+}
+console.log(dictio);
