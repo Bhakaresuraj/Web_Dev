560884b7f45eb8804c7afa4391d24ab3274febcd
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-09 17:41:33 +0530

    modified

--- java/script.js
+++ java/script.js
@@ -1,12 +1,3 @@
-let num=prompt("Enter a number");
-while(true){
-    let guess = prompt("Enter your guess :");
-    if(guess.toUpperCase()=="QUIT"){
-        console.log("Quiting the game");
-        break;
-    }
-    let random =Math.floor(Math.random()* num )+1;
-    if(guess==random){console.log("OMG!!");
-
-    }
+let sum=function(a,b){
+    return a+b;
 }
