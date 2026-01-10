18714fe5b9791a496d71a8a9238a057eef73231c
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-10 10:06:13 +0530

    modified

--- java/script.js
+++ java/script.js
@@ -1,3 +1,11 @@
-let sum=function(a,b){
-    return a+b;
-}
+let length = 4; 
+function callback(){ 
+    console.log(this.length);
+ }
+const object = { 
+    length: 5,
+     method(callback) {
+         callback();
+        }, 
+    };
+object.method(callback, 1, 2); 
