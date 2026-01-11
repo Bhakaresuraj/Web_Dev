99db1e260c56430998ca1773024a929dca987dbb
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-11 20:02:08 +0530

    modified

--- java/script.js
+++ java/script.js
@@ -1,11 +1,12 @@
-let length = 4; 
-function callback(){ 
-    console.log(this.length);
- }
-const object = { 
-    length: 5,
-     method(callback) {
-         callback();
-        }, 
-    };
-object.method(callback, 1, 2); 
+let arr=[6,7,5,96,3];
+console.log(...arr);
+
+// let ans= arr.every((el)=>(el%10==0));
+// console.log(ans);
+console.log(arr.reduce((ans,el)=>{
+    if(ans>el){
+        return el;
+    }else{
+        return ans;
+    }
+}))
