0b178e63a6f813c6e0802fa6387d75c6bd30e0d0
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-12 17:54:03 +0530

    completed javascript pat 8

--- java/script.js
+++ java/script.js
@@ -1,6 +1,31 @@
-let name=["s","u","r","a","j"];
-let [fir,sec,thir,...other]=name;
-console.log(fir);
-console.log(sec);
-console.log(thir);
-console.log(other);
+// function mergeObj(obj1,obj2){
+//     return {...obj1,...obj2};
+// }
+// const stud={
+//     name:"suraj",
+//     age: 30
+// }
+// const obj2={
+//     gen:"male",
+//     city:"Pune"
+// }
+// let mergedObject =mergeObj(stud,obj2);
+
+
+
+// let arr=[1,2,3,4];
+// function doublearr(arr,...args){
+//     let temp =args.map((el)=>el*2);
+//     return [...arr,...temp];
+// }
+// let ano=doublearr(arr,7,5,4,8);     
+
+
+// let arr=["suraj","yash","Sudarshan","Dipak"];
+// let newarr=arr.map((el)=>el.toUpperCase());
+
+let arr=[1,2,3,4];
+// let newarr=arr.map((el)=>el+5);
+
+let s_sum=arr.reduce((res ,el)=>((el**2)+res));
+let avg=arr.reduce((res ,el)=>((el)+res))/arr.length;
