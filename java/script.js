4b491c0193727b4954b56d9842279ccc6ac567c3
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-13 11:42:02 +0530

    modified

--- java/script.js
+++ java/script.js
@@ -1,7 +1,14 @@
-let smallImg = document.getElementsByClassName("oldImg");
-for (k of smallImg) {
-    console.dir(` img link " ${k.src} "change to  " http://127.0.0.1:5500/java/assets/spiderman_img.png "B`);
-    k.src = "http://127.0.0.1:5500/java/assets/spiderman_img.png"
+let para=document.querySelector("p");
+console.dir(para);
+// console.
+para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+para.innerHTML=`<b>Suraj Somath Bhakare </b>. I live inn Malwadi Takali Haji Tal- <b>"Shirur"</b> `
 
+let heading=document.querySelector("h1");
+heading.innerHTML=`<u> ${heading.innerText} </u>`;
+let links= document.querySelectorAll('.box a');
+for(link of links){
+    link.style.color='yellow';
 }
-
