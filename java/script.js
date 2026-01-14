b5f0f614dda05ad07abd1485d5cc86503ac90d8d
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-14 08:34:59 +0530

    modified

--- java/script.js
+++ java/script.js
@@ -1,14 +1,19 @@
-let para=document.querySelector("p");
-console.dir(para);
-// console.
-para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-para.innerHTML=`<b>Suraj Somath Bhakare </b>. I live inn Malwadi Takali Haji Tal- <b>"Shirur"</b> `
+// let para=document.querySelector("p");
+// console.dir(para);
+// // console.
+// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
+// para.innerHTML=`<b>Suraj Somath Bhakare </b>. I live inn Malwadi Takali Haji Tal- <b>"Shirur"</b> `
 
-let heading=document.querySelector("h1");
-heading.innerHTML=`<u> ${heading.innerText} </u>`;
-let links= document.querySelectorAll('.box a');
-for(link of links){
-    link.style.color='yellow';
-}
+// let heading=document.querySelector("h1");
+// heading.innerHTML=`<u> ${heading.innerText} </u>`;
+// let links= document.querySelectorAll('.box a');
+// for(link of links){
+//     link.style.color='yellow';
+// }
+
+
+
+let img= document.querySelector("img");
+console.dir(img.classList);
