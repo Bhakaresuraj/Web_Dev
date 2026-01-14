ccd2c1774d4e37cd35c5081a1f95df8efe10b82d
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-14 12:29:22 +0530

    javascript part 9 is completed (assignment answers are here. ).

--- java/script.js
+++ java/script.js
@@ -1,32 +1,30 @@
-// let para=document.querySelector("p");
-// console.dir(para);
-// // console.
-// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-// para.innerText =`Suraj Somath Bhakare . I live inn Malwadi Takali Haji Tal- "Shirur" `
-// para.innerHTML=`<b>Suraj Somath Bhakare </b>. I live inn Malwadi Takali Haji Tal- <b>"Shirur"</b> `
-
-// let heading=document.querySelector("h1");
-// heading.innerHTML=`<u> ${heading.innerText} </u>`;
-// let links= document.querySelectorAll('.box a');
-// for(link of links){
-//     link.style.color='yellow';
-// }
-
-
-
-// let img= document.querySelector("img");
-// console.dir(img.classList);
-
-// let h4 = document.querySelector('h4');
-
-// let img=document.querySelector("img");
-
 let btn=document.createElement('button');
-btn.style.backgroundColor="red";
+btn.classList.add("button");
+btn.innerText="Click me";
+document.body.append(btn);
 
-let para=document.querySelector('p');
-para.insertAdjacentElement("afterbegin",btn);
-para.insertAdjacentElement("beforebegin",btn);
-para.insertAdjacentElement("beforeend",btn);
-para.insertAdjacentElement("afterend",btn);
+let input=document.createElement('input');
+input.classList.add('input');
+document.body.append(input);
+
+input.setAttribute("placeholder","username");
+btn.setAttribute("id","btn");
+// btn.setAttribute("class","btn");
+
+let butt=document.querySelector("#btn");
+butt.style.backgroundColor='red';
+butt.style.color="white";
+
+
+
+let h1=document.createElement('h1');
+
+h1.innerHTML="<u> DOM Practice .!</u>";
+document.body.append(h1);
+h1.style.color="purple";
+
+
+let p=document.createElement('p');
+p.innerHTML="“Apna College <b> Delta </b> Practice”"
+document.body.append(p);
+p.style.color="orange"
