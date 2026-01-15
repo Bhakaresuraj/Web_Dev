36d3e42114901fe9fe0bc0a49c15b3a5e11d1723
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-15 08:48:35 +0530

    Activity on the event listners

--- java/script.js
+++ java/script.js
@@ -1,7 +1,11 @@
-let btn = document.querySelectorAll("button");
-for (let el of btn) {
-    el.onmouseenter = () => {
-        console.log("hover")
-    };
+let btn=document.querySelectorAll("button");
+for(button of btn){
+
+    button.addEventListener("mouseenter",()=>{
+        console.log("hello Suraj ...!");
+    })
+    button.addEventListener("click",()=>{
+        button.style.backgroundcolor="red";
+    })
 
 }
