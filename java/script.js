d4f407bfc4d7e11243ab41baaa75823a8d72cc83
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-15 11:40:13 +0530

    form events learned

--- java/script.js
+++ java/script.js
@@ -1,15 +1,4 @@
-let btn = document.querySelectorAll("button");
-for(el of btn){
-    el.addEventListener("mouseenter",function (e){
-        this.style.backgroundColor="red";
-        console.log(el.innerText);
-        console.log(e);
-
-    })
-    el.addEventListener("pointerout",function (e){
-        console.log(e);
-        this.style.backgroundColor="grey";
-        console.warn("Mouse moved");
-    })
-}
-
+let inp = document.querySelector("input");
+inp.addEventListener("submit",function(e){
+    console.log("form submitted successfully.. ")
+})
