62f45d4ce8572177feeb55d6691f8df6fe9b6a32
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-19 23:16:34 +0530

    resolve and reject in promice

--- java/script.js
+++ java/script.js
@@ -1,31 +1,10 @@
-function savedata(data, success, failure) {
-    let speed = Math.floor(Math.random() * 10) + 1;
-    if (speed >= 4) {
-        success(data);
-    } else {
-        failure(data);
-    }
-}
-savedata("abc",
-    (Data) => {
-        console.log(` ${Data} is saved successfully 1.`);
-        savedata("apana college",
-            (data) => {
-                console.log(`${data} is saved successfully 2.`);
-                savedata("tupana college",
-                    (data) => {
-                        console.log(`${data} is saved successfully 3.`)
-                    },
-                    (data) => {
-                        console.log(`Weak connection . ${data} is not  saved.`)
-                    }
-                )
-            },
-            (data) => {
-                console.log(`Weak connection . ${data} is not  saved.`)
-            })
-    },
-    (Data) => {
-        console.log(`weak connection : ${Data} is not saved`);
+function savedata(data){
+    return new Promise((resolve,reject) =>{
+        let speed=Math.floor(Math.random()*10)+1;
+        if(speed>4){
+            resolve("Success");
+        }else{
+            reject("Failure not completed.");
+        }
     })
-
+}
