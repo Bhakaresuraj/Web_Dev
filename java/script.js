f711bb00093dba7d7f25d3cfe7956ef4b2aa3b21
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-20 10:40:01 +0530

    promise methods .then and .catch learned also their results and error messages are get known

--- java/script.js
+++ java/script.js
@@ -1,10 +1,32 @@
-function savedata(data){
-    return new Promise((resolve,reject) =>{
-        let speed=Math.floor(Math.random()*10)+1;
-        if(speed>4){
-            resolve("Success");
-        }else{
-            reject("Failure not completed.");
+function savedata(data) {
+    return new Promise((resolve, reject) => {
+        let speed = Math.floor(Math.random() * 10) + 1;
+        if (speed > 4) {
+            resolve("Success data saved.(result)");
+        } else {
+            reject("Weak Connection..(error)");
         }
     })
 }
+
+
+savedata("abc").then((result) => {
+    console.log("Done", result);
+    // console.log(result);
+
+    return savedata("kbc");
+})
+    .then((result) => {
+        console.log("Done", result);
+        // console.log(result);
+        return savedata("hello");
+    })
+    .then((result) => {
+        console.log("Done", result);
+        // console.log(result);
+    })
+    .catch((error) => {
+        console.log("Not saved due to ", error);
+        // console.log(error);
+
+    })
