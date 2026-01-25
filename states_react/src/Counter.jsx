450a21363e17071363969bc66311f5f39d45c164
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-25 18:46:21 +0530

    created Counter successfully

--- states_react/src/Counter.jsx
+++ states_react/src/Counter.jsx
@@ -1,8 +1,16 @@
-function Counter(){
-    let count=0;
-       return (
+import { useState } from "react";
+function Counter() {
+    // let count=0;
+    let [statevariable,setstatevariable]=useState(10);
+    let [count,setCount]=useState(0);
+    function incCount(){
+        setCount(count+=1);
+        console.log(count);
+    }
+    // console.log(arr);
+    return (
         <div>
-            <h3>count = {count}</h3>
+            <h3>count ={count}</h3>
             <button onClick={incCount}>Increase Count</button>
         </div>
     )
