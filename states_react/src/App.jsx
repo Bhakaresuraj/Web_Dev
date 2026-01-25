c37593e2aee77a3304a588000f7fa68261f76b29
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-25 19:29:25 +0530

    Like button created successfully

--- states_react/src/App.jsx
+++ states_react/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-import Counter from "./Counter.jsx"
+// import Counter from "./Counter.jsx"
+import Like from "./LikeButt.jsx"
 function App() {
 
   return (
     <>
     <h1>States in react</h1>
-    <Counter></Counter>
+    <Like></Like>    
+    
+    
+    
+    {/* <Counter></Counter> */}
     </>
   )
 }
--- states_react/src/LikeButt.jsx
+++ states_react/src/LikeButt.jsx
@@ -1,7 +1,28 @@
-function LikeButt(){
-    return(
+import { useState } from "react";
+function LikeButt() {
+    let toggleLike = () => {
+        console.log("toggleLike")
+        let newValue = !isLiked;
+        console.log(newValue);
+        { newValue ? setlikes(likes + 1) : setlikes(likes); }
+        setisLiked(newValue);
+    }
+    let [isLiked, setisLiked] = useState(false);
+    let [likes, setlikes] = useState(0);
+    function countLikes() {
+    }
+    let likeStyle = {
+        color: "red"
+    }
+    return (
         <div>
-            <p>Like Button</p>
+            <p onClick={toggleLike}>
+                {
+                    isLiked ? <i style={likeStyle} className="fa-solid fa-heart"></i> : <i className="fa-regular fa-heart"></i>
+                }
+
+            </p>
+            <p>{likes}</p>
         </div>
     )
 }
