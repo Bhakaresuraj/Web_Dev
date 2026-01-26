c751978b6861538b9e25c9cc7ff64df11d0571c5
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-26 19:54:47 +0530

    created todo list using array of objects in react

--- states_react/src/App.jsx
+++ states_react/src/App.jsx
@@ -2,16 +2,16 @@ import './App.css'
 // import Counter from "./Counter.jsx"
 // import Like from "./LikeButt.jsx"
 // import Render from "./Rendering.jsx"
-
-import LudoBoard from './Ludo_board';
+// import LudoBoard from './Ludo_board';
+import Todo from "./Todo.jsx"
 
 function App() {
 
   return (
     <>
-      <h1>Ludo game</h1>
-
-      <LudoBoard></LudoBoard>
+      <Todo></Todo>
+      {/* <h1>Ludo game</h1> */}
+      {/* <LudoBoard></LudoBoard> */}
       {/* <Render></Render> */}
       {/* <Like></Like>     */}
       {/* <Counter></Counter> */}
---
+++ states_react/src/Todo.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+function Todo() {
+    let [Todo, updateTodo] = useState([]);
+    let [newTask, setnewTask] = useState("");
+
+    let addNewTask = () => {
+        updateTodo((prevTodo) => {
+            return [...prevTodo, { task: newTask, id: uuidv4() }]
+        });
+        setnewTask("");
+    }
+    let getNewTodo = (event) => {
+        setnewTask(() => {
+            return event.target.value;
+        })
+    }
+    let deleteTodo = (id) => {
+        updateTodo((prevTodo) =>(Todo.filter((prevTodo)=>(prevTodo.id != id))));
+
+    }
+
+    return (
+        <div>
+            <input type="text" value={newTask} onChange={getNewTodo} placeholder="Enter your task ...!" />
+            <br /><br />
+            <button onClick={addNewTask}>Add Task</button>
+
+            <br /><br /><br /><br />
+            <hr />
+            <h1>Tasks To Do</h1>
+            <ul>
+                {
+                    Todo.map((el) => (<li key={el.id}>
+                        <span>{el.task}</span>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button onClick={() =>deleteTodo(el.id)}>delete</button>
+                    </li>))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Todo;
