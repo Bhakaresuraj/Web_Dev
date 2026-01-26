8229ca1f56686d2f95f1bbe2f45c0c24dbe5cf39
Bhakaresuraj <surajbhakare190@gmail.com>
2026-01-26 16:46:42 +0530

    learned how to use object as a state variable in react java script

--- states_react/src/App.jsx
+++ states_react/src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 // import Counter from "./Counter.jsx"
 // import Like from "./LikeButt.jsx"
-import Render from "./Rendering.jsx"
+// import Render from "./Rendering.jsx"
+
+import LudoBoard from './Ludo_board';
 
 function App() {
 
   return (
     <>
-    <h1>States in react</h1>
-    <Render></Render>
-    
-    
-    {/* <Like></Like>     */}
-    {/* <Counter></Counter> */}
+      <h1>Ludo game</h1>
+      <LudoBoard></LudoBoard>
+
+      {/* <Render></Render> */}
+      {/* <Like></Like>     */}
+      {/* <Counter></Counter> */}
     </>
   )
 }
---
+++ states_react/src/Ludo_board.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+function ludoBoard() {
+    // let [blue,setBlue]=useState(0);
+    // let [yellow,setYellow]=useState(0);
+    // let [green,setGreen]=useState(0);
+    // let [red,setRed]=useState(0);
+
+    // insted of writing four lines we can simply create this by passing object
+
+    let [moves, setMoves] = useState({ blue: 0, yellow: 0, green: 0, red: 0 })
+
+    let updateBlue = () => {
+        setMoves((prevVal) => {
+            return { ...prevVal, blue: prevVal.blue + 1 }
+        });
+        console.log(moves);
+    }
+    let updateYellow = () => {
+        setMoves((prevVal) => {
+            return { ...prevVal, yellow: prevVal.yellow + 1 }
+        });
+        console.log(moves);
+    }
+    let updateGreen = () => {
+        setMoves((prevVal) => {
+            return { ...prevVal, green: prevVal.green + 1 }
+        });
+        console.log(moves);
+    }
+    let updateRed = () => {
+
+        setMoves((prevVal) => {
+            return { ...prevVal, red: prevVal.red + 1 }
+        });
+        console.log(moves);
+    }
+
+
+    return (
+        <div>
+            <h3>Game begins</h3>
+            <div className="board">
+                <p>blue moves = {moves.blue}</p>
+                <button onClick={updateBlue} style={{ backgroundColor: "blue" }}>+1</button>
+                <p>yellow moves ={moves.yellow}</p>
+                <button onClick={updateYellow} style={{ backgroundColor: "yellow", color: "black" }}>+1</button>
+                <p>green moves ={moves.green}</p>
+                <button onClick={updateGreen} style={{ backgroundColor: "green" }}>+1</button>
+                <p>red moves ={moves.red}</p>
+                <button onClick={updateRed} style={{ backgroundColor: "red" }}>+1</button>
+            </div>
+        </div >
+    )
+}
+
+export default ludoBoard;
