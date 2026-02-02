547219378fcbe09297e10ad1c1835a28070c7cb7
Bhakaresuraj <surajbhakare190@gmail.com>
2026-02-02 09:47:54 +0530

    modified

--- Lotary_game/src/Game.jsx
+++ Lotary_game/src/Game.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Game.css"
 import Ticket from "./Ticket.jsx"
-import { genTicket ,sum } from "./helper.jsx";
+import { genTicket, sum } from "./helper.jsx";
 export default function Lotary({ n = 3, WinSum = 15 }) {
 
     let [ticket, setticket] = useState(genTicket(n));
@@ -14,7 +14,7 @@ export default function Lotary({ n = 3, WinSum = 15 }) {
             <h1>Lotary</h1>
             <Ticket ticket={ticket} wining={iswinning}></Ticket>
             <h3 >{iswinning && "Congratulations you won Lotary....!"}</h3>
-            <button onClick={buyTicket} className="GenTic">Generete Bew Ticket</button>
+            <button onClick={buyTicket} className="GenTic">{iswinning ? "Congratulations !!!" : "Generete Bew Ticket"}</button>
 
         </div>
     )
