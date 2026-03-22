646f1a65ac62a08a7dbac21772736f2080bd81ee
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-22 17:03:25 +0530

    Handling POST request in express  -learned how to handel post request in the express  -learned how to read urencoded and json data in express using middlewares

--- Backend/Miscellaneous/backend/index.js
+++ Backend/Miscellaneous/backend/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 let port = 3000;
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.listen(port, () => {
     console.log(`Server is Running at port ${port}`);
 })
 
 
 app.get("/register", (req, res) => {
-    let {user,pass}=req.query;
+    let { user, pass } = req.query;
     res.send(`Standered get respoce . Welcome ${user}`)
 })
 app.post("/register", (req, res) => {
-    res.send("Standered POST respoce")
+    let {user,password} = req.body;
+    console.log(user);
+    console.log(password);
+    res.send(`Standered post respoce . Welcome ${user   }`);
 })
