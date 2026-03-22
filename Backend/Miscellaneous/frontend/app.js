58948d346be5b9f2c50c29aa1f2935cc2d51a261
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-22 17:43:00 +0530

    Object Prototype  - learned about the prototypes of the object  - how caan we access the object prototype  - __proto__ , Array.prototype

---
+++ Backend/Miscellaneous/frontend/app.js
@@ -0,0 +1,22 @@
+
+/*
+ // --------OBJECT PROTOTYPE--------------------------------------
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4];
+
+arr1.sayhello = () => {
+    console.log("Hello i am arr.....");
+}
+arr1.__proto__.push = (n) => { console.log(`Adding element ${n}`) }
+arr2.sayhello = () => {
+
+    console.log("Hello i am arr.....");
+
+}
+
+console.log(Array.prototype);
+console.log(String.prototype);
+
+*/
+
+
