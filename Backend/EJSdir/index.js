3c795ba92a44a5a95c5bcee381b5d4aa63095a9f
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-22 10:48:42 +0530

    Instagram Page using ejs loop and conditionals
    -developed simple instagram account information render page using ejs loop and conditionals

--- Backend/EJSdir/index.js
+++ Backend/EJSdir/index.js
@@ -3,23 +3,51 @@
 
 
 
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+
 // -----------views directory in ejs----------------
-const express=require("express");
-const app=express();
-const port=3000;
-const path=require("path");
-app.listen(port,()=>{
+const path = require("path");
+app.listen(port, () => {
     console.log("App is listening .....");
 });
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 
-app.set("views",path.join(__dirname,"/views"));
+app.set("views", path.join(__dirname, "/views"));
 
-app.get("/home",(req,res)=>{
-    res.send("THis is home");
+app.get("/home", (req, res) => {
+    res.send("This is home");
 })
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("home.ejs");
 })
 
+
+// ---------------roll dice --------
+// ----Passing data to ejs-----------
+app.get("/rolldice", (req, res) => {
+    let data = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", { data });
+})
+
+
+
+// ------------instagram EJS template------------
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    // let followers=['suraj','yash','supriya','omkar'];
+    const instadata = require("./data.json");
+    let data = instadata[username];
+    console.log(data);
+    if (data) {
+        res.render("suraj.ejs", { data });
+    }
+    else{
+        res.render("error.ejs");
+    }
+})
