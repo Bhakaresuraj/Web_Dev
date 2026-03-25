149a12c394ac7182edf7300e4d94ef2ecba4d035
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-25 19:58:54 +0530

    Created Index.ejs - created route for showing detail all posts

--- Backend/REST_APIs_Practice/index.js
+++ Backend/REST_APIs_Practice/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+// unique id packare...
 const { v4: uuidv4 } = require("uuid");
+
+// path package 
+const path=require("path"); 
 let port = 3000;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express .static(path.join(__dirname,"/public/css")));
 
 app.listen(port, () => {
     console.log("Server is running ...");
@@ -15,6 +23,8 @@ let posts = [{ id: uuidv4(), name: "suraj", content: "hello" },
 { id: uuidv4(), name: "Yash", content: "Hi" }
 ];
 
-app.get("/posts", () => {
-    app.render("index.ejs");
+// index page render .....
+app.get("/posts", (req,res) => {
+    res.render("index.ejs",{posts});
 })
+
