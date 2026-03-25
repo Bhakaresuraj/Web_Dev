768b80932b3727b6d3e2702e393a22f393add943
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-25 20:27:33 +0530

    created Show.ejs file - created route for see detail button but it is not working

--- Backend/REST_APIs_Practice/index.js
+++ Backend/REST_APIs_Practice/index.js
@@ -4,12 +4,12 @@ const app = express();
 const { v4: uuidv4 } = require("uuid");
 
 // path package 
-const path=require("path"); 
+const path = require("path");
 let port = 3000;
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-app.use(express .static(path.join(__dirname,"/public/css")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public/css")));
 
 app.listen(port, () => {
     console.log("Server is running ...");
@@ -24,7 +24,16 @@ let posts = [{ id: uuidv4(), name: "suraj", content: "hello" },
 ];
 
 // index page render .....
-app.get("/posts", (req,res) => {
-    res.render("index.ejs",{posts});
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+})
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+
+    res.send(post);
+
 })
 
