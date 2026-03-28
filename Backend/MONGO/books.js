13a54a3f6a1ed93f49cb86faaa25eb8ca7cf4d08
Bhakaresuraj <surajbhakare190@gmail.com>
2026-03-28 20:39:55 +0530

    Custom Validation errors defining in mongoose  -learned how to define and print custom validation errors.

--- Backend/MONGO/books.js
+++ Backend/MONGO/books.js
@@ -20,40 +20,57 @@ const bookSchema = new mongoose.Schema({
         type: String
     },
     price: {
-        type: Number
+        type: Number,
+        min: [1, "The price of book is very low ...."]
     }
 });
 
 
 const Book = mongoose.model("Book", bookSchema);
 
+// Book.insertOne({
+//     title: "Psychology Of Money",
+//     author: "Margan Hawjel",
+//     price: -200
+
+// }).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// Book.insertOne({
+//     title: "Psychology Of Money",
+//     author: "Margan Hawjel",
+//     price: 250
+
+// }).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+// Book.insertOne({
+//     title: "Psychology Of Money",
+//     author: "Margan Hawjel"
+
+// }).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+
+
+
+// ------------------------Custom error printing and defining .............
+
 Book.insertOne({
     title: "Psychology Of Money",
     author: "Margan Hawjel",
-    price: 250
+    price: -200
 
 }).then((res) => {
     console.log(res);
 }).catch((err) => {
-    console.log(err);
-});
-
-Book.insertOne({
-    title: "Psychology Of Money",
-    author: "Margan Hawjel",
-    price: 250
-
-}).then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
-});
-Book.insertOne({
-    title: "Psychology Of Money",
-    author: "Margan Hawjel"
-
-}).then((res) => {
-    console.log(res);
-}).catch((err) => {
-    console.log(err);
+    console.log(err.errors.price.properties.message);
 });
