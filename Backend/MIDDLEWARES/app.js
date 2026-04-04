16615045d651996703e2b5146c93463598a3cb87
Bhakaresuraj <surajbhakare190@gmail.com>
2026-04-04 11:45:02 +0530

    Next in Moddleware
     -learned next() function in middleware which is used to pass control to next middleware

--- Backend/MIDDLEWARES/app.js
+++ Backend/MIDDLEWARES/app.js
@@ -1,7 +1,33 @@
-const express=require("express");
-const app=express();
-const port=8080;
+const express = require("express");
+const app = express();
+const port = 8080;
 
-app.listen(port,()=>{
+
+
+app.listen(port, () => {
     console.log("Server is working.....!");
-}); 
+});
+
+
+
+app.use((req, res, next) => {
+    console.log("Hi, I am  2nd middleware.");
+    // res.send("Middleware finished...");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("Hi, I am 1st middleware.");
+    // res.send("Middleware finished...");
+    next();
+});
+
+
+app.get("/", (req, res) => {
+    console.log("Hello , this is route ");
+    res.send("Server is working .......!");
+});
+
+
+
+
+
